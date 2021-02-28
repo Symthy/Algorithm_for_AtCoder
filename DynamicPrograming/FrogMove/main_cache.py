f35969f833137c2cd45718860d06362f5449fe2c
Satoshi Yamashita <sympathia3104@gmail.com>
2021-02-28 23:57:05 +0900

    add: bellman-ford

--- DynamicPrograming/FrogMove/main_cache.py
+++ DynamicPrograming/FrogMove/main_cache.py
@@ -5,25 +5,26 @@ high_cost = [2, 9, 4, 5, 1, 6, 10]
 num = len(high_cost)
 dp = [sys.maxsize] * num
 
+
 def rec(i):
-  if i == 0:
-    return 0
+    if i == 0:
+        return 0
 
-  ret = rec(i - 1) + abs(high_cost[i] - high_cost[i - 1])
+    ret = rec(i - 1) + abs(high_cost[i] - high_cost[i - 1])
 
-  if i > 1:
-    comp_val = rec(i - 2) + abs(high_cost[i] - high_cost[i - 2])
-    ret = comp_val if ret > comp_val else ret
+    if i > 1:
+        comp_val = rec(i - 2) + abs(high_cost[i] - high_cost[i - 2])
+        ret = comp_val if ret > comp_val else ret
 
-  dp[i] = ret
-  return ret
+    dp[i] = ret
+    return ret
 
 
 def main():
-  dp[0] = 0
-  result = rec(num - 1)
-  print(dp)
-  print(result)
+    dp[0] = 0
+    result = rec(num - 1)
+    print(dp)
+    print(result)
 
 
 main()
---
+++ ShortestPathProblem/Bellman-Ford/main.py
@@ -0,0 +1,68 @@
+from typing import TypeVar, List, Dict
+
+INF = int(float('inf'))
+
+
+class Edge:
+    def __init__(self, to: int, weight: int):
+        self.to: int = to
+        self.weight: int = weight
+
+
+# 緩和処理
+def choose_min(vertex1: int, vertex2: int) -> int:
+    if vertex1 > vertex2:
+        return False
+    return True
+
+
+def main():
+    print('入力(頂点数 辺数 始点):')
+    vertex, edge, start = input()
+    vertex_num = int(vertex)
+    edge_num = int(edge)
+    start_num = int(start)
+
+    print('グラフ入力')
+    graph: List[List[Edge]] = []
+    for i in range(vertex_num):
+        graph.append([])
+    for i in range(edge_num):
+        print('頂点(from) 頂点(to) 重み:')
+        v1, v2, w = input()
+        e = Edge(int(v2), int(w))
+        graph[v1].append(e)
+
+    is_exist_negative_cycle: bool = False
+    vertex_cost_dict: Dict[int, int] = {i: INF for i in range(vertex_num)}
+    vertex_cost_dict = {start_num: 0}
+    for i in range(vertex_num):
+        is_update = False
+        for v in range(vertex_num):
+            if vertex_cost_dict[v] == 0:
+                continue
+            for e in graph[v]:
+                # 緩和処理
+                if choose_min(vertex_cost_dict[e.to], vertex_cost_dict[v] + e.weight):
+                    is_update = True
+                    vertex_cost_dict[e.to] = vertex_cost_dict[v] + e.weight
+
+        if not is_update:
+            # 更新が行われなかったら終了(最短経路が求まっている)
+            break
+        if i == vertex_num - 1 and is_update:
+            # 頂点数回目で更新が行われた場合は負閉路
+            is_exist_negative_cycle = True
+
+    # result output
+    if is_exist_negative_cycle:
+        print('Negative Cycle')
+        return
+    for v in range(vertex_num):
+        if vertex_cost_dict[v] < INF:
+            print(vertex_cost_dict[v])
+        else:
+            print('INF')
+
+
+main()
