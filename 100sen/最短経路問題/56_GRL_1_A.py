dd73954bfccf318c4cd7ff6cef451e07bd394fa0
Satoshi Yamashita <sympathia3104@gmail.com>
2021-06-10 23:38:36 +0900

    add: 56 GRL 1 A (dijkstra)

---
+++ 100sen/最短経路問題/56_GRL_1_A.py
@@ -0,0 +1,53 @@
+"""
+単一始点最短経路
+https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A&lang=ja
+"""
+from heapq import heappop, heappush
+from typing import Dict, List, TypeVar
+
+INF = float('inf')
+
+
+class Edge:
+    def __init__(self, to: int, weight: int):
+        self.to: int = to
+        self.weight: int = weight
+
+
+def dijkstra():
+    # V:頂点数, E:辺数, r:始点
+    print('入力(頂点数 辺数 始点):')
+    V, E, r = map(int, input().split())
+    # s,t:辺(有向), d:重み
+
+    graph: List[List[Edge]] = []
+    for i in range(V):
+        graph.append([])
+    for i in range(E):
+        print('頂点(from) 頂点(to) 重み:')
+        s, t, w = map(int, input().split())
+        e = Edge(t, w)
+        graph[s].append(e)
+
+    que = []
+    heappush(que, (0, r))  # used_vertex_listの代わりにもなる
+    vertex_cost: Dict[int, int] = {i: INF for i in range(V)}
+    vertex_cost[r] = 0
+    while que:
+        # 使用済みでない最小コストの頂点を探す
+        min_cost, min_vertex_index = heappop(que)
+
+        # min_vertex を始点に緩和処理
+        for e in graph[min_vertex_index]:
+            if vertex_cost[e.to] > min_cost + e.weight:
+                vertex_cost[e.to] = min_cost + e.weight
+                heappush(que, (vertex_cost[e.to], e.to))
+
+    for cost in vertex_cost.values():
+        if cost == INF:
+            print("INF")
+        else:
+            print(cost)
+
+
+dijkstra()
--- sample/ShortestPathProblem/Dijkstra/simple/main.py
+++ sample/ShortestPathProblem/Dijkstra/simple/main.py
@@ -31,42 +31,42 @@ def output(vertex_num: int, vertex_cost: Dict[int, int]):
 
 def main():
     print('入力(頂点数 辺数 始点):')
-    vertex, edge, start = input()
-    vertex_num = int(vertex)
-    edge_num = int(edge)
-    start_num = int(start)
+    vertex_num, edge_num, start_num = map(int, input().split())
 
     print('グラフ入力')
     graph: List[List[Edge]] = []
     for i in range(vertex_num):
         graph.append([])
     for i in range(edge_num):
-        print('頂点(from) 頂点(to) 重み:')
-        v1, v2, w = input()
-        e = Edge(int(v2), int(w))
+        # print('頂点(from) 頂点(to) 重み:')
+        v1, v2, w = map(int, input().split())
+        e = Edge(v2, w)
         graph[v1].append(e)
 
     # ダイクストラ
     used_vertex: Dict[int, bool] = {i: False for i in range(vertex_num)}
-    vertex_cost: Dict[int, int] = {i: INF for i in range(vertex_num)}
-    vertex_cost[start_num] = 0
+    vertex_index_cost: Dict[int, int] = {i: INF for i in range(vertex_num)}
+    vertex_index_cost[start_num] = 0
 
-    for i in range(vertex):
+    for _ in range(vertex_num):
         # 使用済みでない最小コストの頂点を探す
+        # ヒープキュー使えば早くなる。要素追加/最小要素取り出し:O(logN)
         min_cost = INF
-        min_vertex = -1
-        for v, cost in vertex_cost.items():
-            if not v in used_vertex and cost < min_cost:
-                min_cost = vertex_cost[v]
-                min_vertex = v
-
-        if min_vertex == -1:
+        min_vertex_index = -1
+        for v_index, cost in vertex_index_cost.items():
+            if not v_index in used_vertex and cost < min_cost:
+                min_vertex_index = v_index
+                min_cost = vertex_index_cost[min_vertex_index]
+        if min_vertex_index == -1:
             break
 
         # min_vertex を始点に緩和処理
-        for e in graph[min_vertex]:
-            if choose_min(vertex_cost[e.to], vertex_cost[min_vertex] + e.weight):
-                vertex_cost[e.to] = vertex_cost[v] + e.weight
-        used_vertex[min_vertex] = True
+        for e in graph[min_vertex_index]:
+            if choose_min(vertex_index_cost[e.to], min_cost + e.weight):
+                vertex_index_cost[e.to] = min_cost + e.weight
+        used_vertex[min_vertex_index] = True
 
-    output(vertex_num, vertex_cost)
+    output(vertex_num, vertex_index_cost)
+
+
+main()
