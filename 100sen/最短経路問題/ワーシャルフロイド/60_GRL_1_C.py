ec0d7e40b2d2cf2b54f14319c5e482c11ebb152b
Satoshi Yamashita <sympathia3104@gmail.com>
2021-06-15 23:47:19 +0900

    refactor: Warshall–Floyd

--- 100sen/最短経路問題/ワーシャルフロイド/60_GRL_1_C.py
+++ 100sen/最短経路問題/ワーシャルフロイド/60_GRL_1_C.py
@@ -4,6 +4,7 @@
 https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C&lang=ja
 """
 
+from math import isinf
 from typing import List, TypeVar
 
 INF = float('inf')
@@ -13,42 +14,52 @@ def main():
     # V:頂点数、E:辺数
     V, E = map(int, input().split())
 
-    graph: List[List] = []
-    for i in range(V):
-        row: List = []
-        for j in range(V):
-            if i == j:
-                row.append(0)
-            else:
-                row.append(INF)
-        graph.append(row)
+    # graph: List[List] = []
+    # for i in range(V):
+    #     row: List = []
+    #     for j in range(V):
+    #         if i == j:
+    #             row.append(0)
+    #         else:
+    #             row.append(INF)
+    #     graph.append(row)
+    graph: List[List] = [[float('inf') for i in range(V)] for j in range(V)]
 
-    # s:始点, t:終点, w:重み
+   # s:始点, t:終点, w:重み
     for _ in range(E):
         s, t, w = map(int, input().split())
         graph[s][t] = w
+    for i in range(V):
+        graph[i][i] = 0
 
     for k in range(V):
         for i in range(V):
             for j in range(V):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+                if graph[i][j] > graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
 
     # output
-    output: List[str] = []
+    # output: List[str] = []
+    # for i in range(V):
+    #     if any(((cost < 0 for cost in graph[i]))):
+    #         print("NEGATIVE CYCLE")
+    #         break
+    #     row: List = []
+    #     for j in range(V):
+    #         if graph[i][j] == INF:
+    #             row.append('INF')
+    #         else:
+    #             row.append(str(graph[i][j]))
+    #     output.append(' '.join(row))
+    # for r in output:
+    #     print(r)
     for i in range(V):
-        if any(((cost < 0 for cost in graph[i]))):
-            print("NEGATIVE CYCLE")
-            break
-        row: List = []
-        for j in range(V):
-            if graph[i][j] == INF:
-                row.append('INF')
-            else:
-                row.append(str(graph[i][j]))
-        output.append(' '.join(row))
+        if graph[i][i] < 0:
+            print('NEGATIVE CYCLE')
+            exit()
 
-    for r in output:
-        print(r)
+    for graph_row in graph:
+        print(' '.join(('INF' if isinf(elem) else str(elem) for elem in graph_row)))
 
 
 main()
