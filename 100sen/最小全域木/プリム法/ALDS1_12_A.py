510e2d5df78b5386cd9dd84dd90e612d2c8135e1
Satoshi Yamashita <sympathia3104@gmail.com>
2021-06-21 00:27:01 +0900

    add: Prim

---
+++ 100sen/最小全域木/プリム法/ALDS1_12_A.py
@@ -0,0 +1,47 @@
+"""
+プリム法
+https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_12_A&lang=jp
+
+入力例
+5
+-1 2 3 1 -1
+2 -1 -1 4 -1
+3 -1 -1 1 1
+1 4 1 -1 3
+-1 -1 1 3 -1
+"""
+
+
+from heapq import heapify, heappop, heappush
+
+
+def main():
+    # 頂点数 n
+    n = int(input())
+
+    adjacency_matrix = []   # 隣接行列のリスト
+    for _ in range(n):
+        row = list(map(int, input().split()))
+        adjacency_matrix.append([(cost, i) for i, cost in enumerate(row) if cost != -1])
+    visited_vertex = [0] * n
+    connect_count = 0
+    que = [(0, 0)]    # (cost, vertex_index)
+    heapify(que)
+    result_cost = 0
+    while True:
+        cost, i = heappop(que)
+        if visited_vertex[i]:
+            continue
+
+        visited_vertex[i] = 1
+        connect_count += 1
+        result_cost += cost
+
+        # 新たに繋げたノードから行けるところをエンキュー
+        for tup in adjacency_matrix[i]:
+            heappush(que, tup)
+
+        if connect_count >= n:
+            break
+    print(result_cost)
+    return
